8eb3c8cb95bcc216705681059c4feb4cc18d1f5c
Allwyen <allwyen27@gmail.com>
2019-08-14 12:48:00 +0530

    nodejs day2 morning session

--- Expressjs/app.js
+++ Expressjs/app.js
@@ -2,17 +2,27 @@ const Express = require("express");
 
 var app = new Express();
 
-app.get('/',(req,res)=>{
-    res.send("Welcome to my Express Website");
-});
+app.set('view engine','ejs'); //to use template engine in express. we can also use here react,handlebars etc
 
 /*
     '/' is my primary route which means through '/' "welcome to my website" is accessed and we need
     request and response model hence (req,res).
 */
 
-app.get('/home',(req,res)=>{
-    res.send("Welcome to my Home page");
+app.get('/',(req,res)=>{
+    res.render('index',{'title':'ICTAK'});
 });
 
-app.listen(3369);  //web can assign any number.3000 is commonly used.
+/* 
+    app.get('/home',(req,res)=>{
+        res.send("Welcome to my ICTAK Home");
+    }); 
+*/ 
+
+app.get('/home',(req,res)=>{
+    res.render('home',{'title':'ICTAK:Home','names':["Allwyen","Amal","Joel","Romy"]});
+});
+
+app.listen(3369,()=>{
+    console.log("Server running on port::3369...");
+});  //we can assign any number.3000 is commonly used.
