c70f6df793fc43537f4f988cb71dcaaadc5f4c10
Allwyen <allwyen27@gmail.com>
2019-08-14 10:29:41 +0530

    nodejs day2 morning session

--- Nodejs/event.js
+++ Nodejs/event.js
@@ -2,8 +2,23 @@ const EventEmitter = require('events');
 
 var emitter = new EventEmitter();
 
+/*
 emitter.on("doorOpen",()=>{
     console.log("Ring Ring...");
 });
 
 emitter.emit("doorOpen");
+*/
+
+emitter.on("doorOpen",(name)=>{
+    console.log(name+" is waiting outside...");
+});
+
+emitter.on("doorOpen",(name)=>{
+    console.log("Dear "+name+" please wait outside for a minute...");
+});
+
+//there can be one or more listeners for a single emit like wise there can also be multiple emitters
+
+emitter.emit("doorOpen","Allwyen");
+emitter.emit("doorOpen","Sebastian");
